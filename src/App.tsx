a463a14b6059bf28b4fcbf964a8184c51f549597
Dhruv B Shetty <dhruvbshetty@gmail.com>
2026-01-10 22:51:30 +0200

    Remove BrowserRouter from App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Magazin from "./pages/Magazin";
 import NotFound from "./pages/NotFound";
@@ -16,7 +16,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
         <Header />
         <ScrollToSection /> 
         <main className="pt-2">
@@ -26,7 +25,6 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
